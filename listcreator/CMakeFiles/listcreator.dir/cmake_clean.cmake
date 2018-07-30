f156b20c1bef04ba9bc1632ce0b46b317f58bbed
Kihyeon Park <kihyeon.park@swmobile.co.kr>
2018-07-30 17:58:47 +0900

    first commit for studying opencv

---
+++ listcreator/CMakeFiles/listcreator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/listcreator.dir/imagelist_creator.cpp.o"
+  "listcreator.pdb"
+  "listcreator"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/listcreator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
